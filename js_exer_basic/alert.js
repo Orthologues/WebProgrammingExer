9c7f85e5ad49402e1697f4c49e9f1c016f469932
jiaweizhao <zaapzungsyugei@gmail.com>
2020-09-25 02:38:12 +0200

    2020-09-24 update

---
+++ js_exer_basic/alert.js
@@ -0,0 +1,12 @@
+// In macOS:
+// ⌘ means Command.
+// ⌥ means Option (also called “Alt”)
+// ⌃ means Control.
+// ⇧ means Shift.
+
+// In order to open snippets in chrome:
+// cmd[ctrl]+alt+J -> ">>" -> sources -> ">>" to the right of "Pages" -> snippets -> New snippet
+
+// this script in currently used in bootstrap/TinDog_exer/index.html
+var test_alert = "This website uses Bootstrap4 and FontAwesome";
+alert(test_alert);

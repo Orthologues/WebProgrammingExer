ca748cfeffd9182efc787b819af54ad79b6bfbe6
jiawei <zaapzungsyugei@gmail.com>
2020-09-25 17:37:04 +0200

    exer in js 2020-09-25

--- js_exer_basic/alert.js
+++ js_exer_basic/alert.js
@@ -8,5 +8,46 @@
 // cmd[ctrl]+alt+J -> ">>" -> sources -> ">>" to the right of "Pages" -> snippets -> New snippet
 
 // this script in currently used in bootstrap/TinDog_exer/index.html
-var test_alert = "This website uses Bootstrap4 and FontAwesome";
-alert(test_alert);
+
+function input_name() {
+  var test_prompt = prompt("Please type your name");
+  var len_test_prompt = test_prompt.length;
+  var len_remaining = 140 - len_test_prompt;
+  // capitalize the first letter and decapitalize all other letters
+  var first_letter_upper = test_prompt.slice(0, 1).toUpperCase();
+  test_prompt = first_letter_upper + test_prompt.slice(1, len_test_prompt).toLowerCase();
+
+  if (len_remaining < 0) {
+    test_prompt = test_prompt.slice(0, 140);
+    alert("You have typed in more than 140 characters, you have 0 characters left.");
+  } else {
+    alert("You have typed in " + len_test_prompt + " characters, you have " + len_remaining + " characters left.");
+    //sample: You have typed in 5 characters, you have 135 characters left.
+  }
+  alert("Your name is: " + test_prompt);
+}
+
+function isLeap(year){
+  var remainder_4=year%4;
+  var remainder_100=year%100;
+  var remainder_400=year%400;
+  if(remainder_4==0){
+    if(remainder_100==0){
+      if(remainder_400==0){
+        return "Leap year."
+      }else{
+        return "Not leap year.";
+      }
+    }else{
+      return "Leap year.";
+    }
+  }else{
+    return "Not leap year.";
+  }
+}
+
+function main(){
+  // input_name();
+}
+
+main();

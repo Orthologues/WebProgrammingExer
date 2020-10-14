a8bd8e0d3c018beac09538cb3079cdc744b2a3f3
jiaweizhao <zaapzungsyugei@gmail.com>
2020-10-14 09:13:54 +0200

    use body-parser to calculate BMI

--- node_exer_basic/express_calculator_srv/calculator_srv.js
+++ node_exer_basic/express_calculator_srv/calculator_srv.js
@@ -3,14 +3,53 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require("body-parser");
+const jsdom = require('jsdom');
+
+
+function parseData_DOM(html) {
+  const {
+    JSDOM
+  } = jsdom;
+  const dom = new JSDOM(html);
+  const $ = (require('jquery'))(dom.window);
+  // to be continued
+}
 
 // __dirname means the current folder
 // add static files to route
 app.use(express.static(path.join(__dirname, 'src')));
 
+// use body-parser to parse form request
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 // "/" means root route of the listened port
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, 'src/index.html'));
+});
+
+function calculate_BMI(height, weight) {
+  height = height / 100;
+  let BMI = weight / Math.pow(height, 2);
+  // if height or weight isn't a number, then BMI isn't a number, throw err
+  if (isNaN(BMI)) {
+    throw 'Invalid input';
+  } else {
+    // round BMI to 2 numbers after dot
+    return BMI.toFixed(2).toString();
+  }
+}
+
+// calculate BMI by parsing form request
+app.post("/", function(req, res) {
+  try {
+    let BMI = calculate_BMI(req.body.height, req.body.weight);
+    res.send(BMI);
+  } catch (e) {
+    res.send("Your input should be number!");
+  } finally {}
 });
 
 app.listen(3500, function() {

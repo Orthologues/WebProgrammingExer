d348b96dbcd57a8206acdb4fa8b3e03222e14534
jiaweizhao <zaapzungsyugei@gmail.com>
2020-10-30 02:20:41 +0100

    add data validation for schema

--- nodejs_exer/weather_api_srv/lib/mongoose_record.js
+++ nodejs_exer/weather_api_srv/lib/mongoose_record.js
@@ -4,43 +4,72 @@ import mongoose from 'mongoose';
 const mongoUrl = 'mongodb://localhost:27017';
 
 export default async (weatherInfo, systemTime) => {
-  mongoose.connect(`${mongoUrl}/openWeatherAPI`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-
-  // define an array to store all pieces of array ${weatherInfo}
-  const weatherRecords = [];
-  const weatherSchema = new mongoose.Schema({
-    cityName: String,
-    systemTime: String,
-    temperature: String,
-    feelsLike: String,
-    humidity: String,
-    wind: String,
-    visibility: String,
-    iconUrl: String
-  });
-  // create a new model under ${dbName} using ${weatherSchema}, using ${systemTime} as the name of the new collection
-  const weatherModel = mongoose.model(systemTime, weatherSchema);
-
-  weatherInfo.forEach((weatherRecord) => {
-    // define information from ${weatherRecord} to model ${weather}
-    const weather = new weatherModel({
-      cityName: weatherRecord[0],
-      systemTime: weatherRecord[1],
-      temperature: weatherRecord[2],
-      feelsLike: weatherRecord[3],
-      humidity: weatherRecord[4],
-      wind: weatherRecord[5],
-      visibility: weatherRecord[6],
-      iconUrl: weatherRecord[7]
+  if (weatherInfo.length > 0) {
+    mongoose.connect(`${mongoUrl}/openWeatherAPI`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
     });
-    weatherRecords.push(weather);
-  });
 
-  // insert information of array ${weatherRecords} to ${weatherCollection} in db
-  weatherModel.insertMany(weatherRecords, (err) => {
-    throw new Error('Error during \'insertMany\'!');
-  });
+    // define an array to store all pieces of array ${weatherInfo}
+    const weatherRecords = [];
+    const weatherSchema = new mongoose.Schema({
+      cityName: {
+        type: String,
+        // add data validation
+        required: [true, 'City name missing']
+      },
+      systemTime: {
+        type: String,
+        required: [true, 'System time missing']
+      },
+      temperature: {
+        type: String,
+        required: [true, 'Temperature missing']
+      },
+      feelsLike: {
+        type: String,
+        required: [false, 'Felt-like temperature data missing']
+      },
+      humidity: {
+        type: String,
+        required: [true, 'Humidity data missing']
+      },
+      wind: {
+        type: String,
+        required: [true, 'Wind data missing']
+      },
+      visibility: {
+        type: String,
+        required: [false, 'Visibility data missing']
+      },
+      iconUrl: {
+        type: String,
+        required: [true, 'weatherIconUrl missing']
+      }
+    });
+    // create a new model under ${dbName} using ${weatherSchema}, using ${systemTime} as the name of the new collection
+    const weatherModel = mongoose.model(systemTime, weatherSchema);
+
+    weatherInfo.forEach((weatherRecord) => {
+      // define information from ${weatherRecord} to model ${weather}
+      const weather = new weatherModel({
+        cityName: weatherRecord[0],
+        systemTime: weatherRecord[1],
+        temperature: weatherRecord[2],
+        feelsLike: weatherRecord[3],
+        humidity: weatherRecord[4],
+        wind: weatherRecord[5],
+        visibility: weatherRecord[6],
+        iconUrl: weatherRecord[7]
+      });
+      weatherRecords.push(weather);
+    });
+
+    // insert information of array ${weatherRecords} to ${weatherCollection} in db
+    weatherModel.insertMany(weatherRecords, (err) => {
+      throw new Error('Error during \'insertMany\'!');
+    });
+  } else {
+    throw new Error('No successful queries during this round');
+  }
 }

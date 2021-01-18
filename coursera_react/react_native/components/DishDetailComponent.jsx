1fc1c17451a82802af9d86281b5a7e886694298d
jiaweizhao <zaapzungsyugei@gmail.com>
2021-01-18 16:30:31 +0100

    react-native gesture(2) v3.3.2

--- coursera_react/react_native/components/DishDetailComponent.jsx
+++ coursera_react/react_native/components/DishDetailComponent.jsx
@@ -85,70 +85,86 @@ function RenderComments({comments}) {
     );
 }
 
-function RenderDish({dish, favorite, onFavoritePress, onCommentPress}) {
+class RenderDish extends Component {
 
-    const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
+    constructor(props) {
+        super(props);
+        this.recognizeDrag = this.recognizeDrag.bind(this);
+        this.handleViewRef = this.handleViewRef.bind(this);
+    }
+
+    recognizeDrag = ({ moveX, moveY, dx, dy }) => {
         if ( dx < -200 )
             return true;
         else
             return false;
     }
 
-    const panResponder = PanResponder.create({
+    handleViewRef = ref => this.view = ref;
+
+    panResponder = PanResponder.create({
+       
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
         },
+        onPanResponderGrant: () => {
+            this.view.rubberBand(1000).
+            then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));},
         onPanResponderEnd: (e, gestureState) => {
             console.log("pan responder end", gestureState);
-            if (recognizeDrag(gestureState))
-                Alert.alert(
-                    'Add Favorite',
-                    'Are you sure you wish to add ' + dish.name + ' to favorite?',
-                    [
-                    {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-                    {text: 'OK', onPress: () => {props.favorite ? console.log('Already favorite') : props.onPress()}},
-                    ],
-                    { cancelable: false }
-                );
+            if (this.recognizeDrag(gestureState))
+              Alert.alert(
+                'Add Favorite',
+                'Are you sure you wish to add ' + this.props.dish.name + ' to favorite?',
+                [
+                {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                {text: 'OK', onPress: () => {this.props.favorite ? 
+                    console.log('Already favorite') : this.props.onFavoritePress()}},
+                ],
+                { cancelable: false }
+              );
 
             return true;
         }
     })
 
-    if (dish != null) {
-        return(
-        <View style={styles.formRow}>
-          <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
-          {...panResponder.panHandlers}>
-            <Card
-            featuredTitle={dish.name}
-            image={{uri: baseUrl + dish.image}}>
-              <Text style={{margin: 10}}>
-                  {dish.description}
-              </Text>
-              <Icon
-                  raised
-                  reverse
-                  name={ favorite ? 'heart' : 'heart-o'}
-                  type='font-awesome'
-                  color='#f50'
-                  onPress={() => favorite ? console.log('Already favorite') : onFavoritePress()}
-                  />
-              <Icon
-                  raised
-                  reverse
-                  name={ 'pencil' }
-                  type='font-awesome'
-                  color='#8A2BE2'
-                  onPress={() => onCommentPress()}
-                  />
-            </Card>
-          </Animatable.View>
-        </View>     
-        );
-    }
-    else {
-        return(<View></View>);
+    render() {
+        if (this.props.dish != null) {
+            return(
+            <View style={styles.formRow}>
+              <Animatable.View animation="fadeInDown" duration={2000} delay={1000} 
+              ref={this.handleViewRef} {...this.panResponder.panHandlers}>
+                <Card
+                featuredTitle={this.props.dish.name}
+                image={{uri: baseUrl + this.props.dish.image}}>
+                  <Text style={{margin: 10}}>
+                      {this.props.dish.description}
+                  </Text>
+                  <Icon
+                      raised
+                      reverse
+                      name={ this.props.favorite ? 'heart' : 'heart-o'}
+                      type='font-awesome'
+                      color='#f50'
+                      onPress={() => this.props.favorite ? 
+                        console.log('Already favorite') : this.props.onFavoritePress()}
+                      />
+                  <Icon
+                      raised
+                      reverse
+                      name={ 'pencil' }
+                      type='font-awesome'
+                      color='#8A2BE2'
+                      onPress={() => this.props.onCommentPress()}
+                      />
+                </Card>
+              </Animatable.View>
+            </View>     
+            );
+        }
+        else {
+            return(<View></View>);
+        }
     }
 }
 

593707acacc27e7743e73bb46134aab568abd63b
jiaweizhao <zaapzungsyugei@gmail.com>
2021-05-12 23:52:32 +0200

    v4.3.1 expo-image-picker

--- coursera_react/react_native/components/MainComponent.jsx
+++ coursera_react/react_native/components/MainComponent.jsx
@@ -88,20 +88,21 @@ const ContactNavigator = createStackNavigator({
 });
 
 const LoginNavigator = createStackNavigator({
-  Login: { screen: Login }
+  Login: Login
 }, {
-navigationOptions: ({ navigation }) => ({
-  headerStyle: {
-      backgroundColor: "#512DA8"
-  },
-  headerTitleStyle: {
-      color: "#fff"            
-  },
-  headerTintColor: "#fff",
-  headerLeft: <Icon name="menu" size={24}
-    iconStyle={{ color: 'white' }} 
-    onPress={ () => navigation.toggleDrawer() } />    
-})
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    title: 'Login',
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }} 
+      onPress={ () => navigation.toggleDrawer() } />    
+  })
 });
 
 const ReservationNavigator = createStackNavigator({

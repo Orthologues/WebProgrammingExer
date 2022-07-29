43d61d06f69e985053982e2914e26eb8b8ed1544
Issac <kitahara1869@163.com>
2022-07-29 11:27:11 +0200

    Revert "Added the form to update todos" since next/router doesn't work
    This reverts commit 1a59d53d521c25b202c48ddea68b9b0714b3b401.

--- nextjs_ts_exer/todolist/components/TodoList.tsx
+++ nextjs_ts_exer/todolist/components/TodoList.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Todo } from '../generated/graphql-frontend';
-import Link from 'next/link';
-import UpdateTodoForm from './UpdateTodoForm'
 
 interface Props {
     todos: Todo[];
@@ -14,11 +12,7 @@ const TodoList: React.FC<Props> = ({ todos }) => {
           todos.map(todo => {
             return (
               <li key={todo.id} className="todo-list-item">
-                <Link href="/update/[id]" as={`/update/${todo.id}`}>
-                  <a className="todo-list-item-title">
-                    <UpdateTodoForm initialValues={{title: todo.title}} />
-                  </a>
-                </Link>
+                {todo.title} ({todo.status})
               </li>
             );
           })

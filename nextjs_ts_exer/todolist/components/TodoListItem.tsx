b9135946ebb52233b0e26971b43763891f22db33
Issac <kitahara1869@163.com>
2022-07-31 00:30:11 +0200

    Marking the todos as completed

--- nextjs_ts_exer/todolist/components/TodoListItem.tsx
+++ nextjs_ts_exer/todolist/components/TodoListItem.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import Link from 'next/link';
 import { Todo, useDeleteTodoMutation } from '../generated/graphql-frontend';
 import { Reference } from '@apollo/client';
-import { defaultFieldResolver } from 'graphql';
+import { useUpdateTodoMutation, TodoStatus } from '../generated/graphql-frontend';
 
 interface Props {
     todo: Todo;
@@ -43,9 +43,31 @@ const TodoListItem: React.FC<Props> = ({ todo }) => {
             alert('An error occurred, please try again.');
         }
     }, [error]);
+    
+    const [updateTodo, { loading: updateLoading, error: updateError}] = useUpdateTodoMutation({
+        errorPolicy: 'all'
+    })
+
+    const handleStatusChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newStatus = e.target.checked ? TodoStatus.Completed : TodoStatus.Active;
+        updateTodo({ variables: { input: {id: todo.id, status: newStatus} } })
+    }
+
+    useEffect(() => {
+        if (updateError) {
+            alert('An error occurred. Please try again!')
+        }
+    }, [updateError])
 
     return (
         <li className="todo-list-item">
+          <label className="checkbox">
+              <input type="checkbox" onChange={handleStatusChange}
+                  checked={todo.status === TodoStatus.Completed}
+                  disabled={updateLoading}
+              />
+              <span className="checkbox-mark">&#10003;</span>
+          </label>
           <Link href={`/update/${encodeURIComponent(todo.id)}`}>
             <a className="todo-list-item-title">{todo.title}</a>
           </Link>

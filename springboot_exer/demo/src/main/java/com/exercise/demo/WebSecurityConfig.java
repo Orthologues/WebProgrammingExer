ef6e43bc7a1b628dc6c69d0627121df93be7d796
jiaweizhao <zaapzungsyugei@gmail.com>
2022-08-14 22:56:17 +0200

    Enabled a rudimentary JPA H2 in-memory DB at /h2-console

--- springboot_exer/demo/src/main/java/com/exercise/demo/WebSecurityConfig.java
+++ springboot_exer/demo/src/main/java/com/exercise/demo/WebSecurityConfig.java
@@ -26,7 +26,10 @@ public class WebSecurityConfig {
             .antMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN").and()
         .requestCache().requestCache(new NullRequestCache()).and()
         .cors().and()
-        .csrf().disable();
+        .csrf().disable(); // it's recommended to disable CSRF when non-browser clients are expected
+        // By default Spring Security disables rendering within an iframe 
+        // to enable same-origin requests to "localhost:8080/h2-console" which runs within an iframe
+        httpSecurity.headers().frameOptions().sameOrigin();
         return httpSecurity.build();
     }
 
--- springboot_exer/demo/src/main/java/com/exercise/demo/users/PrimaryUser.java
+++ springboot_exer/demo/src/main/java/com/exercise/demo/users/PrimaryUser.java
@@ -1,40 +1,33 @@
 package com.exercise.demo.users;
 
-import java.util.Date;
-
-import javax.validation.constraints.Past;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
-@ApiModel(description="All details about a USD user account ")
-public class User {
+@Entity
+@Table(name = "PRIMARY_USERS")
+@ApiModel(description="All details about a DKK user account ")
+public class PrimaryUser {
 
+    @Id
+    @GeneratedValue
     private Integer id; // could be either int or null
-
     //@Size to validate an attribute's value between the attributes min and max
+
     @Size(min=2, max=20, message="Name should have between 2-20 characters!")
     @ApiModelProperty(notes="Name should have between 2-20 characters!")
     private String name;
-
-    @Past(message="The registration date must be in the past!")
-    @ApiModelProperty(notes="The registration date must be in the past!")
-    private Date registrationDate;
-    
     private String signedUpDate;
 
-    private double USD;
-
-    protected static final double minAmount = 4.99;
-    protected static final double maxAmount = Math.pow(10, 8);
-
-    public User(Integer id, String name, Date registrationDate, String signedUpDate, double USD) {
+    public PrimaryUser(Integer id, String name, String signedUpDate) {
         this.id = id;
         this.name = name;
-        this.registrationDate = registrationDate;
         this.signedUpDate = signedUpDate;
-        this.USD = USD;
     }    
 
     public Integer getId() {
@@ -53,14 +46,6 @@ public class User {
         this.name = name;
     }
 
-    public Date getRegistrationDate() {
-        return this.registrationDate;
-    }
-
-    public void setRegistrationDate(Date registrationDate) {
-        this.registrationDate = registrationDate;
-    }
-
     public String getSignedUpDate() {
         return this.signedUpDate;
     }
@@ -68,18 +53,10 @@ public class User {
     public void setSignedUpDate(String signedUpDate) {
         this.signedUpDate = signedUpDate;
     }
-
-    public double getUSD() {
-        return this.USD;
-    }
-
-    public void setUSD(double USD) {
-        this.USD = USD;
-    }
     
     @Override
     public String toString() {
-        return String.format("Bean [id=%s, name=%s, registrationDate=%s owns %.2f USD]", id, name, registrationDate, USD);
+        return String.format("Bean [id=%s, name=%s, signedUpDate=%s]", id, name, signedUpDate);
     }
 
 }
